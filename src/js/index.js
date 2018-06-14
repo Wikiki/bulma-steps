f271d2a678204d0b41715c5c0fb9e00e13c07fd5
Wikiki <wikiki@protonmail.com>
2018-06-14 17:37:00 +0200

    fix:

--- src/js/index.js
+++ src/js/index.js
@@ -54,10 +54,10 @@ export default class bulmaSteps extends EventEmitter {
   init() {
     this._id = 'bulmaSteps' + (new Date()).getTime() + Math.floor(Math.random() * Math.floor(9999));
     
-    this.steps = element.querySelectorAll(this.options.selector);
-    this.contents = element.querySelectorAll(this.options.selector_content);
-    this.previous_btn = element.querySelector(this.options.previous_selector);
-    this.next_btn = element.querySelector(this.options.next_selector);
+    this.steps = this.element.querySelectorAll(this.options.selector);
+    this.contents = this.element.querySelectorAll(this.options.selector_content);
+    this.previous_btn = this.element.querySelector(this.options.previous_selector);
+    this.next_btn = this.element.querySelector(this.options.next_selector);
 
     [].forEach.call(this.steps, (step, index) => {
       step.setAttribute('data-step-id', index);
